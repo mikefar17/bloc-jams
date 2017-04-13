5fbfb5c6c32d9c036691aceeb0d0f5dcff3b4834
Michael Farias <michaelfarias75@gmail.com>
2017-04-13 09:56:21 -0700

    Correct Checkpoint 11

--- scripts/landing.js
+++ scripts/landing.js
@@ -6,7 +6,7 @@
                 points[index].style.opacity= 1;
                 points[index].style.transform="scaleX(1) translateY(0)";
                 points[index].style.msTransform= "scaleX(1) translateY(0)";
-                points[index].style.WebkitTransform = "scaleX(1) translate(0)"
+                points[index].style.WebkitTransform = "scaleX(1) translateY(0)"
             }
             for(var i= 0; i<points.length; i++) {
                 revealPoint(i);
