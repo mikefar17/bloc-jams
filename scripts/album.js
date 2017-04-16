3c4902aed1d8f244a5cb6af243ff93019809f2f2
Michael Farias <michaelfarias75@gmail.com>
2017-04-15 20:01:53 -0700

    Checkpoint 12 dom scripting album view: completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,3 +28,64 @@ var albumMarconi = {
         { title: 'Wrong phone number', duration: '2:15'}
     ]
 };
+
+var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+     // below this allows us to access the data held in the attribute using dom method when the mouse leaves the table row
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber +'</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+     return template;
+ };
+
+var setCurrentAlbum = function(album) {
+     // #1
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+     // #2
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
+ 
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+ // #1 variable targets table
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+// #2 if mousehovers ontop of item then console logs the event
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1 if event target's parent class name equals album view...
+         if(event.target.parentElement.className === 'album-view-song-item') {
+             // Do this... 
+          event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     
+     for (var i= 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event){
+             //Revert the content back to the number
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number')
+         });
+     }
+ }
